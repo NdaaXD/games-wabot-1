3b131cdb5b505e8ce0cec295d4fdc8104e4ef440
NdaaXD <98279739+NdaaXD@users.noreply.github.com>
2022-03-04 21:22:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -52,8 +52,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot Is The Best'
+global.packname = ''
+global.author = ''
 
 global.multiplier = 39 // The higher, The harder levelup
 
